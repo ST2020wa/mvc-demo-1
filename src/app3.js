dfa2264bd7dd799ebad700c3fce29d38b4a14699
ST <shaotian.li.april@gmail.com>
2020-09-10 15:14:32 +0800

    init

---
+++ src/app3.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+import './app3.css'
+
+const $square = $('#app3 .square')
+
+$square.on('click', ()=>{
+    $square.toggleClass('active')  //  jquery特有的api，toggleclass，如果没有就加上，如果有就删掉
+})
